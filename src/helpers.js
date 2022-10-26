0b993ba73675d2847df29f44bd2fcf2c847bde72
Garrett McQuigg <garrettmcquigg@gmail.com>
2022-10-26 18:16:18 -0500

    practice

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+function choice(items) {
+  let res = Math.floor(Math.random() * items.length);
+  return items[res];
+}
+
+function remove(item, items) {
+  for (let i = 0; i <= items.length; i++) {
+    if (items[i] === item) {
+      return [...items.slice(0, i), ...items.slice(i + 1)];
+    }
+  }
+}
+
+export { choice, remove };
